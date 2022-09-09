130e0146ab89b4229428b0038fae4a84832f75f3
Fatimah Mohammed <fmg1413@hotmail.com>
2022-09-09 07:22:55 +0300

    input function

---
+++ input.py
@@ -0,0 +1,8 @@
+# take data from user
+# name = input('Enter your name:')
+# print(name)
+
+# take number from user
+age = input('Enter your age:')
+# print('You Are ' + age + ' years old!')
+print(f'You Are {age} years old!')
