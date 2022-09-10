eef0d97c1b84b39eeb36821849e27916e391e7b3
Fatimah Mohammed <fmg1413@hotmail.com>
2022-09-10 16:41:47 +0300

    define for and while function

--- loops.py
+++ loops.py
@@ -1,6 +1,6 @@
 
 # print from 0 to 4 where the number inside range dooes not printed
-i = 1
+# i is defined as a number of index start from 0
 for i in range(4):
     print(i)
 
@@ -18,3 +18,6 @@ print('----------------------------------------------------------------')
 # start from 2 to range 4
 for i in range(2, 4):
     print(i)
+
+# for loop used to iterate over squance in order
+# while used to check acondition before execute the statment
