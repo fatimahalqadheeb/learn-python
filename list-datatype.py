802a53e674e32481793ff962094795add1730e9b
Fatimah Mohammed <fmg1413@hotmail.com>
2022-09-10 08:48:39 +0300

    list datatype

---
+++ list-datatype.py
@@ -0,0 +1,11 @@
+colors = ['red', 'blue', 'green', 'yellow', 'cyan', 'white']
+print(type(colors))
+
+colors.append('pink')
+print(colors)
+
+colors.insert(1, 'black')
+print(colors)
+
+colors.remove('pink')
+print(colors)
